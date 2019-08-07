4259eb3e2f512ff36a55afecb6574a651e4e0922
Santiago Enciso <whereispolaris@gmail.com>
2019-08-07 14:56:32 -0500

    Reorganized comments to reflect the flow of the app.

--- index.js
+++ index.js
@@ -3,22 +3,18 @@
 // Prompts the user for each guess and keeps track of the user's remaining guesses
 
 var animals = require('./wordBank.js');
-
 var inquirer = require('inquirer');
+var guessedLetters = [];
+
 
 function startGame() {
     console.log("Game has started");
     // Pick  random word
     selectedAnimal = animals[Math.floor(Math.random() * animals.length)];
     console.log(selectedAnimal);
-    // Make a constructor with it?
+    // Use Word constructor to store it
+
     // Ask for letter
-    // if letter is correct
-    // Update constructor
-    // Add to Guessed letters
-    // If letter is incorrect
-    // Add to Guessed letters
-    // -1 to guesses left?
     inquirer
         .prompt([
             /* Pass your questions in here */
@@ -32,6 +28,13 @@ function startGame() {
             // Use user feedback for... whatever!!
             console.log("the guessed letter is:" + answers.guessedLetter);
         });
+    // if letter is correct
+    //      Update constructor
+    //      Add to Guessed letters
+    // If letter is incorrect
+    //      Add to Guessed letters
+    //      -1 to guesses left?
+
 }
 
 startGame();
