efdb8a92314554a70a77d9772edf4bbfd28a3081
Santiago Enciso <whereispolaris@gmail.com>
2019-08-07 19:03:43 -0500

    Updated Letter construction with better logic.

--- Letter.js
+++ Letter.js
@@ -1,15 +1,29 @@
 // Contains a constructor, Letter.This constructor should be able to either display an underlying character or a blank placeholder(such as an underscore), depending on whether or not the user has guessed the letter.That means the constructor should define:
 
-// A string value to store the underlying character for the letter
 
-// A boolean value that stores whether that letter has been guessed yet
+function Letter(wordInput) {
+    // A string value to store the underlying character for the letter 
+    this.letter = wordInput;
+    // A boolean value that stores whether that letter has been guessed yet
+    this.guessedYet = false;
 
-// A function that returns the underlying character if the letter has been guessed, or a placeholder(like an underscore) if the letter has not been guessed
+    // A function that returns the underlying character if the letter has been guessed, or a placeholder(like an underscore) if the letter has not been guessed
+    this.guessedLetterCheck = function () {
+        if (this.guessedYet === true) {
+            return this.letter;
+        }
+        else {
+            return "_";
+        }
+        // Need to write condition for spaces (" "). 
+    }
 
-// A function that takes a character as an argument and checks it against the underlying character, updating the stored boolean value to true if it was guessed correctly
-
-function Letter(input) {
-
+    // A function that takes a character as an argument and checks it against the underlying character, updating the stored boolean value to true if it was guessed correctly
+    this.correctLetterCheck = function (userInput) {
+        if (userInput === this.letter) {
+            this.guessedYet = true;
+        }
+    }
 
 }
 
